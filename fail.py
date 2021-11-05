44ec1e2255f01f7e6f955ad1d939b2f7b2e74167
karlerik-t <karlerik.toomemagi@gmail.com>
2021-11-05 23:50:40 +0200

    b.

--- fail.py
+++ fail.py
@@ -1,8 +1,24 @@
 import os
 from pixboard import *
 from PIL import Image
+import imghdr
 #import Image
 #from matplotlib import pyplot as plt
+
+# Seab kõik failid kaustast järjendisse
+def files(path="."):
+    return os.listdir(path)
+
+# Teeb hulga olemasolevatest piltidest kaustas
+def imgs_in_folder(arg):
+    images = set()
+    for el in arg:
+        img = imghdr.what(el)
+        if img:
+            images.add(el)
+    if len(images):
+        return images
+    return None
 
 def kuvaFailiSisu(fail):
     f = open(fail, "r+", encoding="UTF-8")
@@ -20,31 +36,8 @@ def kustutaFail(fail):
     else:
         print("The file does not exist")
 
-def loetleFailid(path=".", laiend=None):
-    #for subdir, dirs, files in os.walk('./'):
-    #    for file in files:
-    #        print(file)
-
-    sisu = os.listdir(path)
-    leidubFail = False
-    if laiend == None:
-        for x in sisu:
-            print(x)
-    else:
-        for x in sisu:
-            if x.endswith(laiend):
-                leidubFail = True
-                break
-    if leidubFail:
-        for x in sisu:
-            if x.endswith(laiend):
-                print(x)
-
 def kuvaPilt(fail):
     image = Image.open(fail)
     image.show()
     
-
 #kuvaPilt("wut.png")
-
-
